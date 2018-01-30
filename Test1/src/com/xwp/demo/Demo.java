b6948f96a23b34108af51d7583256edd1d969013
xwp <516379501@qq.com>
2018-01-30 17:42:48 +0800

    Initial commit

---
+++ Test1/src/com/xwp/demo/Demo.java
@@ -0,0 +1,10 @@
+package com.xwp.demo;
+
+/**
+ * Created by xwp on 2018/1/3.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.print("Hello World!!!");
+    }
+}
